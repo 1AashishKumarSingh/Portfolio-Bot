e8fb179b89273947c058efd6432f15daa8580aef
1AashishKumarSingh <167994838+1AashishKumarSingh@users.noreply.github.com>
2025-03-24 21:00:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -85,7 +85,5 @@ def search():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000)) 
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
 
